598674df69152b218707d81250f0a300cf690523
Saifuddin Saifee <saifuddin.saifee@live.co.uk>
2023-09-16 01:21:58 +0530

    Optimized

--- script.js
+++ script.js
@@ -4,20 +4,20 @@
 // B5A5FB
 // COC8FC
 
-document.addEventListener("mousemove", function (event) {
-    var cursorShadow = document.querySelector(".cursor-shadow");
-    var cursorSize = 20; // Adjust this value to match the size of your cursor shadow
-  
-    cursorShadow.style.left = event.clientX - cursorSize / 2 + "px";
-    cursorShadow.style.top = event.clientY - cursorSize / 2 + "px";
-  });
-  
-  document.addEventListener("mouseenter", function () {
-    var cursorShadow = document.querySelector(".cursor-shadow");
-    cursorShadow.classList.add("active");
-  });
-  
-  document.addEventListener("mouseleave", function () {
-    var cursorShadow = document.querySelector(".cursor-shadow");
-    cursorShadow.classList.remove("active");
-  });
+// document.addEventListener("mousemove", function (event) {
+//     var cursorShadow = document.querySelector(".cursor-shadow");
+//     var cursorSize = 20; // Adjust this value to match the size of your cursor shadow
+
+//     cursorShadow.style.left = event.clientX - cursorSize / 2 + "px";
+//     cursorShadow.style.top = event.clientY - cursorSize / 2 + "px";
+// });
+
+// document.addEventListener("mouseenter", function () {
+//     var cursorShadow = document.querySelector(".cursor-shadow");
+//     cursorShadow.classList.add("active");
+// });
+
+// document.addEventListener("mouseleave", function () {
+//     var cursorShadow = document.querySelector(".cursor-shadow");
+//     cursorShadow.classList.remove("active");
+// });
